edcccaf1f6017e2e3d47865bd18aefc865c5ca5c
Refdinal <refdinal22@gmail.com>
2021-01-04 11:37:57 +0700

    feat : add crud mahasiswa data

---
+++ admin/mahasiswa_input.php
@@ -0,0 +1,21 @@
+<?php	
+	$nim = $_POST['nim'];
+	$nama = $_POST['nama'];
+	$tingkat = $_POST['tingkat'];
+	$alamat = $_POST['alamat'];
+	$password = $_POST['password'];
+	
+	include "../db_connection.php";
+
+	$query = "INSERT INTO mahasiswa VALUES ('$nim', '$nama', '$tingkat', '$password','$alamat')";
+
+	$res = $conn->query($query);
+	mysqli_close($conn);
+	
+	if($res){
+		header("location:admin_home_page.php?page=mahasiswa");		
+	}
+	else{		
+		header("location:admin_home_page.php?page=mahasiswa&status=error"); 		
+	}	
+?>
---
+++ admin/mahasiswa_update.php
@@ -0,0 +1,21 @@
+<?php	
+	$old_nim = $_POST['old_nim'];
+	$nim = $_POST['nim'];
+	$nama = $_POST['nama'];	
+	$alamat = $_POST['alamat'];	
+	$tingkat = $_POST['tingkat'];	
+	
+	include "../db_connection.php";
+
+	$query = "UPDATE mahasiswa SET NIM='$nim', Nama_Mhs='$nama', Tingkat='$tingkat', Alamat='$alamat' WHERE NIM='$old_nim'";	
+
+	$res = $conn->query($query);
+	mysqli_close($conn);
+	
+	if($res){
+		header("location:admin_home_page.php?page=mahasiswa");		
+	}
+	else{		
+		header("location:admin_home_page.php?page=mahasiswa&status=error"); 		
+	}	
+?>
