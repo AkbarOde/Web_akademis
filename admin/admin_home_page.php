eb7525b3798c145bad9939e9c3dfc770ff9d5af9
Refdinal <refdinal22@gmail.com>
2021-01-08 15:22:06 +0700

    refactor : separating the javascript code from the admin homepage to a file

--- admin/admin_home_page.php
+++ admin/admin_home_page.php
@@ -102,54 +102,9 @@
 		} );
 		$('#list-data').dataTable({
   			aaSorting: []
-		})		
-
-		// Untuk komfirmasi delete data
-		function hapusData(id, identifier){
-			console.log(id);
-	        if (confirm("Apakah anda yakin akan menghapus data ini?")){
-	        	switch(identifier) {
-				  case 1:
-				    window.location.href = 'dosen/dosen_delete.php?id=' + id;
-				    break;
-				  case 2:
-				    window.location.href = 'matkul/matkul_delete.php?id=' + id;
-				    break;
-				  case 3:
-				    window.location.href = 'ruangan/ruangan_delete.php?id=' + id;
-				    break;		
-				  case 4:
-				    window.location.href = 'jadwal/jadwal_mengajar_delete.php?id=' + id;
-				    break;			  
-				}	          
-	        }
-    	}
-    	
-    	function hapusDataJadwal(id_jadwal, id_dosen){    
-	        if (confirm("Apakah anda yakin akan menghapus data ini?")){
-	          window.location.href = 'jadwal/jadwal_mengajar_delete.php?id=' +id_jadwal+'&dosen='+id_dosen;
-	        }
-      	}
-
-    	function hapusDataNilai(nim, id_matkul, tingkat){
-    		console.log(nim);
-	        if (confirm("Apakah anda yakin akan menghapus data ini?")){
-	          window.location.href = 'nilai/nilai_delete.php?nim='+nim+'&id='+id_matkul+'&tingkat='+tingkat;
-	        }
-      	}
-        function hapusDataMhs(nim){
-	        if (confirm("Apakah anda yakin akan menghapus data ini?")){
-	          window.location.href = 'mahasiswa/mahasiswa_delete.php?nim=' + nim;
-	        }
-      	}
-
-      	function show_form(id){
-      		var form = document.getElementById(id);
-
-      		form.style.display = "block";
-      	}
-
+		})			
 	</script>
+	<script type="text/javascript" src="../js/delete_alert.js"></script>
 	<script type="text/javascript" src="../js/modal.js"></script>
 	</body>
 </html>
---
+++ js/delete_alert.js
@@ -0,0 +1,38 @@
+// Untuk komfirmasi delete data
+function hapusData(id, identifier){
+	console.log(id);
+    if (confirm("Apakah anda yakin akan menghapus data ini?")){
+    	switch(identifier) {
+		  case 1:
+		    window.location.href = 'dosen/dosen_delete.php?id=' + id;
+		    break;
+		  case 2:
+		    window.location.href = 'matkul/matkul_delete.php?id=' + id;
+		    break;
+		  case 3:
+		    window.location.href = 'ruangan/ruangan_delete.php?id=' + id;
+		    break;						  		 
+		}	          
+    }
+}
+
+function hapusDataJadwal(id_jadwal, id_dosen, id_mengajar){    
+    if (confirm("Apakah anda yakin akan menghapus data ini?")){
+      window.location.href = 'jadwal/jadwal_mengajar_delete.php?id=' +id_jadwal+'&dosen='+id_dosen+'&mengajar='+id_mengajar;
+    }
+	}
+
+function hapusDataNilai(nim, id_matkul, tingkat){
+	console.log(nim);
+    if (confirm("Apakah anda yakin akan menghapus data ini?")){
+      window.location.href = 'nilai/nilai_delete.php?nim='+nim+'&id='+id_matkul+'&tingkat='+tingkat;
+    }
+	}
+function hapusDataMhs(nim){
+    if (confirm("Apakah anda yakin akan menghapus data ini?")){
+      window.location.href = 'mahasiswa/mahasiswa_delete.php?nim=' + nim;
+    }
+}
+
+
+	
